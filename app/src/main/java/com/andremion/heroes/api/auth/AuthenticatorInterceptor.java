1e759ecaeb2d37574cbef1741f2d545b8c03b65b
André Mion <andremion@gmail.com>
2016-09-26 23:15:52 +0100

    Read Marvel keys from gradle.properties or System environment variables

--- app/src/main/java/com/andremion/heroes/api/auth/AuthenticatorInterceptor.java
+++ app/src/main/java/com/andremion/heroes/api/auth/AuthenticatorInterceptor.java
@@ -1,5 +1,7 @@
 package com.andremion.heroes.api.auth;
 
+import com.andremion.heroes.BuildConfig;
+
 import java.io.IOException;
 
 import okhttp3.HttpUrl;
@@ -9,11 +11,6 @@ import okhttp3.Response;
 
 public class AuthenticatorInterceptor implements Interceptor {
 
-    // TODO: Put your private key here
-    private static final String PRIVATE_KEY = "###";
-    // TODO: Put your public key here
-    private static final String PUBLIC_KEY = "###";
-
     private static final String QUERY_NAME_TIMESTAMP = "ts";
     private static final String QUERY_NAME_APIKEY = "apikey";
     private static final String QUERY_NAME_HASH = "hash";
@@ -22,13 +19,13 @@ public class AuthenticatorInterceptor implements Interceptor {
     public Response intercept(Chain chain) throws IOException {
 
         String ts = String.valueOf(System.currentTimeMillis());
-        String hash = HashHelper.generate(ts + PRIVATE_KEY + PUBLIC_KEY);
+        String hash = HashHelper.generate(ts + BuildConfig.MARVEL_PRIVATE_KEY + BuildConfig.MARVEL_PUBLIC_KEY);
 
         Request currentRequest = chain.request();
 
         HttpUrl url = currentRequest.url().newBuilder()
                 .addQueryParameter(QUERY_NAME_TIMESTAMP, ts)
-                .addQueryParameter(QUERY_NAME_APIKEY, PUBLIC_KEY)
+                .addQueryParameter(QUERY_NAME_APIKEY, BuildConfig.MARVEL_PUBLIC_KEY)
                 .addQueryParameter(QUERY_NAME_HASH, hash).build();
 
         Request newRequest = currentRequest.newBuilder().url(url).build();
